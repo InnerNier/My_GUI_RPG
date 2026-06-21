d2bafcf63b29f9af280c464f5b4b82189c78df27
xande <xande@XanderCPU>
2026-06-20 22:05:48 -0500

    Main_Window updates

--- src/Main/Main_Window.java
+++ src/Main/Main_Window.java
@@ -6,14 +6,54 @@ import java.awt.*;
 public class Main_Window 
 {
 	JFrame Main_Window;
+	
+	JPanel Next_Panel = new JPanel();
+	JLabel Next_Label = new JLabel();
+	JButton Next_Button = new JButton("Next");
+	
 	public Main_Window()
 	{
-		Main_Window = new JFrame("Test");
+		//Frame Set up
+		Main_Window = new JFrame("My Video Game");
 		Main_Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Main_Window.setSize(500, 400);
+		Main_Window.setSize(800, 600);
 		Main_Window.setLayout(new BorderLayout());
 		
-		Main_Window.setVisible(true);
+		//Next Page Specification
+		Next_Panel.setLayout(new BoxLayout(Next_Panel, BoxLayout.Y_AXIS));
+		Next_Label.setAlignmentX(Component.CENTER_ALIGNMENT);
+        Next_Button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        Next_Panel.add(Box.createRigidArea(new Dimension(0, 100)));
+		Next_Panel.add(Next_Label);
+		Next_Panel.add(Box.createRigidArea(new Dimension(0, 10)));
+		Next_Panel.add(Next_Button);
 		
+		//Initial Panel for the Frame
+		Set_Next_Text("Welcome to the Game");
+		Set_Next_Background(Color.BLACK);
+		Add_Next_Panel();
+		
+		//Make it visible
+		Main_Window.setVisible(true);
+	}
+	public void Add_Next_Panel()
+	{
+		Main_Window.add(Next_Panel);
+	}
+	public void Set_Next_Background(Color color)
+	{
+		Next_Panel.setBackground(color);
+	}
+	public void Set_Next_Text(String text)
+	{
+		Next_Label.setFont(new Font("Arial", Font.PLAIN, 24)); 
+		Next_Label.setForeground(Color.RED);
+		Next_Label.setText(text);
+	}
+	public void Clear_JFrame()
+	{
+		Main_Window.removeAll();
+		Main_Window.revalidate();
+        Main_Window.repaint();
 	}
 }
