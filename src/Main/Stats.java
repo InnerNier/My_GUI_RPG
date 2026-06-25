3eac90cf4aad8540821ce657630c5a88256294cb
xande <xande@XanderCPU>
2026-06-24 21:02:24 -0500

    Updates to Story_Script and Stats classes

--- src/Main/Stats.java
+++ src/Main/Stats.java
@@ -1,5 +1,7 @@
 package Main;
 
+import java.util.Random;
+
 public class Stats 
 {
 	int level;
@@ -10,6 +12,8 @@ public class Stats
 	int speed;
 	int defense;
 	int experience;
+	Random random = new Random();
+	final int[] Level_Milestones = {10, 30, 60, 100, 200};
 	public Stats(int level, int hp, int strength, int magic, int speed, int defense, int experience)
 	{
 		this.level = level;
@@ -21,4 +25,65 @@ public class Stats
 		this.defense = defense;
 		this.experience = experience;
 	}
+	public void Take_Damage(int damage, boolean ismagic)
+	{
+		if(ismagic)
+		{
+			damage = damage - this.magic;
+		}
+		else
+		{
+			damage = damage - this.defense;
+		}
+		if(damage < 0)
+		{
+			damage = 0;
+		}
+		this.current_hp = this.current_hp - damage;
+		if(this.current_hp < 0)
+		{
+			this.current_hp = 0;
+		}
+	}
+	public void Heal(int heal)
+	{
+		this.current_hp = this.current_hp + heal;
+		if(this.current_hp > this.max_hp)
+		{
+			this.current_hp = this.max_hp;
+		}
+	}
+	public int Attack()
+	{
+		return this.strength + random.nextInt(5);
+	}
+	public int Magic()
+	{
+		return this.magic + random.nextInt(5);
+	}
+	public void Gain_Experience(int experience)
+	{
+		this.experience = this.experience + experience;
+	}
+	public boolean Can_Level_Up()
+	{
+		if(this.level == 5)
+		{
+			return false;
+		}
+		if(this.experience >= Level_Milestones[level - 1])
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	public void Level_Up()
+	{
+		//Need logic for Level Up Screen
+		//Decide how stats are calculated later
+	}
+	
 }
--- src/Main/Story_Script.java
+++ src/Main/Story_Script.java
@@ -1,11 +1,15 @@
 package Main;
 
+import java.awt.Color;
 import java.awt.event.*;
+import javax.swing.*;
 
 public class Story_Script 
 {
 	final String[] Beginning_Dialogue = {"The party of adventurers are trying to make it through a horde of monsters", "You must guide their actions"};
+	final String[] Monster_Dialogue = {"Werewolf appears", "Wraith appears", "Giant appears"};
 	int iterator = 0;
+	private Color Color;
 	public void Beginning_Dialogue(Main_Window main_window)
 	{
 		iterator = 0;
@@ -17,6 +21,7 @@ public class Story_Script
 				if(iterator == Beginning_Dialogue.length)
 				{
 					main_window.Next_Button.removeActionListener(this);
+					main_window.Set_Next_Text("Charge on!");
 					Exploration_Script(main_window);
 				}
 				else
@@ -31,6 +36,24 @@ public class Story_Script
 	public void Exploration_Script(Main_Window main_window)
 	{
 		iterator = 0;
-		System.out.println("Success");
+		main_window.Set_Next_Background(Color.RED);
+		ActionListener Dialogue_2_Listener = new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				if(iterator == Monster_Dialogue.length)
+				{
+					main_window.Next_Button.removeActionListener(this);
+					System.out.println("Success");
+				}
+				else
+				{
+					main_window.Set_Next_Text(Monster_Dialogue[iterator]);
+					iterator++;
+				}
+			}
+		};
+		main_window.Next_Button.addActionListener(Dialogue_2_Listener);
 	}
 }
